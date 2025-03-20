f97c15f2ff2fada0f0aa0ac54d4426147524bf1e
sannreo <sannidhya@reo.dev>
2025-03-20 12:26:52 +0530

    org data corrected

--- s3_dump.py
+++ s3_dump.py
@@ -95,7 +95,7 @@ def fetch_product_usage(
         and date(p.created_at) >= '{from_date}'::date
         and date(p.created_at) <= '{to_date}'::date
         """)
-        elif activities_data.shape[0] != data_len:
+        elif activities_data.shape[0] < data_len:
             raise ValueError(f"""All the data wasn't pulled from db. Pls check the connector.
     Data to be pulled: {data_len}. Data pulled: {activities_data.shape[0]}
     Here's the query for reference:
@@ -131,7 +131,7 @@ def fetch_product_usage(
             org_datas.append(
                 org_service_conn.run_query(f"""
         select id::text as org_id, company_name, domain, linked_in_url, estimated_num_employees,
-            annual_revenue_printed, total_funding, city, state, country, industry, sub_industry, keywords, tech_masala
+            annual_revenue_printed, total_funding, city, state, country, industry, sub_industry
         from org_entity
         where id in ('{"','".join(org_ids[index:index+org_filter_chunksize])}')
             """, chunk_query=False)
